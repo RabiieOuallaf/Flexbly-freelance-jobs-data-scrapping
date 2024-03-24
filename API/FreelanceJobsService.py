372ee69567384a5e3b395b6c1009f8ab417c11c8
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2024-03-24 14:54:10 +0000

    some changes to the codebae

--- API/FreelanceJobsService.py
+++ API/FreelanceJobsService.py
@@ -4,6 +4,48 @@ import requests
 from bs4 import BeautifulSoup
 
 
+def scrap_google_freelance_jobs(url):
+    headers = {
+        "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"
+    }
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # In case of a non-200 status code , throw an exception
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        jobs_listings = soup.find_all("li", class_="iFjolb gws-plugins-horizon-jobs__li-ed")
+        scraped_jobs = []
+        for job in jobs_listings:
+            title_tag = job.find("div", class_="BjJfJf PUpOsf")
+            title = title_tag.text
+
+            company_tag = job.find("div", class_="vNEEBe")
+            company = company_tag.text
+
+            location_tag = job.find("div", class_="Qk80Jf")
+            location = location_tag.text
+
+            posted_date_span = job.find("span", class_="LL4CDc")
+            posted_date = posted_date_span.text
+
+            link = job.find("a")["href"]
+
+            scraped_data = {
+                "title": title,
+                "company": company,
+                "location": location,
+                "posted_date": posted_date,
+                "link": link
+            }
+            scraped_jobs.append(scraped_data)
+        return scraped_jobs
+
+    except requests.exceptions.RequestException as e:
+        print(f"an error occurred while scrapping indeed freelance jobs : {e}")
+        return []
+
+
 def scrap_freelancer_jobs(filter_param):
     print(filter_param)
     headers = {
@@ -12,6 +54,7 @@ def scrap_freelancer_jobs(filter_param):
 
     try:
         base_url = "https://www.freelancer.com/jobs"
+        redirect_url = "https://www.freelancer.com"
 
         if filter_param:
             query_string = urlencode(filter_param)
@@ -29,6 +72,7 @@ def scrap_freelancer_jobs(filter_param):
             title_tag = job.find("a", class_="JobSearchCard-primary-heading-link")
             title = title_tag.text.replace('\n', '').strip()
             url = title_tag.get("href")
+            print(base_url + url)
 
             posted_date_tag = job.find("span", class_="JobSearchCard-primary-heading-days")
             posted_date = posted_date_tag.text.replace('\n', '').strip()
@@ -40,11 +84,13 @@ def scrap_freelancer_jobs(filter_param):
             job_description = job_description_tag.text.replace('\n', '').strip()
 
             jobs_data = {
+                "source": "freelancer.com",
                 "title": title,
                 "posted_date": posted_date,
                 "price": price,
                 "job_description": job_description,
-                "url": url
+                "url": redirect_url + url
+
             }
 
             scraped_jobs.append(jobs_data)
@@ -52,4 +98,4 @@ def scrap_freelancer_jobs(filter_param):
         return scraped_jobs
     except requests.exceptions.RequestException as e:
         print(f"an error occurred while scrapping indeed freelance jobs : {e}")
-        return []
+        return []~
